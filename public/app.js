9e3ec6a206865a45b3b1d0a446aeb5e0aa68faf7
Eric Perez <ericricardoperez@gmail.com>
2014-03-08 10:12:22 -0800

    backbone javascript for autocomplete

--- public/app.js
+++ public/app.js
@@ -10,11 +10,51 @@ window.App = {
     App.autocompleter = new Autocompleter();
     var ws = new WebSocket('ws://' + window.location.host + window.location.pathname);
     ws.onmessage = function(m) { 
-      autocompleter.add(m.data); 
+      App.autocompleter.add(m.data); 
     };
-
   }
 };
+
+App.Routers.Main = Backbone.Router.extend({
+  router: {
+    "" : 'index'
+  }, 
+
+  initialize: function(){
+    this.view = new App.Views.AutoComplete();
+    this.view.render();
+    $('body').html(this.view.$el);
+  },
+
+  index: function(){
+  }
+
+});
+
+App.Views.AutoComplete = Backbone.View.extend({
+  events: {
+    'keyup #word': 'autoComplete'
+  },
+
+  render: function(){
+    this.$el.html("<input type='text' name='word' id='word'><div id='container'></div>");
+  },
+
+  autoComplete: function(event){
+    var input = $('#word').val();
+    if (input === ""){
+      $('#container').empty();
+    }else {
+      $('#container').empty();
+      var words = App.autocompleter.complete(input);
+      _.each(words, function(word){
+        $('#container').append('<h3>'+ word +'</h3>');
+      });
+    }
+  }
+
+});
+
 $(document).ready(function(){
   App.initialize();
 });
